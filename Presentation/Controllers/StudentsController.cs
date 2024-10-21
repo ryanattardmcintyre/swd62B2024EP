eabe92d233e045bc00a4309989617197c057747b
Ryan Attard <ryanattard@gmail.com>
2024-10-21 14:19:01 +0200

    Update implemented

--- Presentation/Controllers/StudentsController.cs
+++ Presentation/Controllers/StudentsController.cs
@@ -51,13 +51,49 @@ namespace Presentation.Controllers
         {
             //get the object pertaining to the student idcard no = id, and pass it to the View, for the admin to be able to edit the details
 
-
-            return View(); 
+            var student = _studentRepository.GetStudent(id);
+            if (student == null)
+            {
+                /*
+                 * Session: works only on the server side, that you can pass data to and from controllers and it will be available as long as the session
+                 * ViewData: works on client side, however it is available to use only from controller to view; with redirection data in it is lost
+                 * ViewBag: is like ViewData, only difference is how to set the data; it is set by variable declaration on the fly
+                 * TempData: is like ViewData, but it survives ONE redirection
+                 * 
+                 */
+                TempData["error"] = "Id provided doesn't exist";
+                return RedirectToAction("Index");
+            }
+             
+                //it will look for a View with name Edit.cshtml inside Views>Students>.... and it will load and pass the object
+                return View(student);
         }
 
         [HttpPost] //this is called after the admin submits the updated changes
         public IActionResult Edit(Student updatedStudent) 
-        { return View(); }
+        {
+            if (updatedStudent == null)
+            {
+                TempData["error"] = "Data passed is not valid";
+                return Redirect("Error");
+            }
+
+            //this is the place where you apply validation
+
+            ModelState.Remove("Group");//it removes the Group navigational property from the list to check
+
+            if (ModelState.IsValid) //triggers any validators which you may have coded
+            {
+                _studentRepository.UpdateStudent(updatedStudent);
+                TempData["message"] = "Student is saved in database";
+
+                //redirecting the user where?
+                return RedirectToAction("Index");
+            }
+
+            return View(updatedStudent); 
+        
+        }
 
          
     }
